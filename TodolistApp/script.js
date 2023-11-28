81034d6dfadbb267754262d91cea221e1881f178
mjshin0425 <junis0425@gmail.com>
2023-11-28 23:46:49 +0900

    [UPDATE] 체크 표시, 삭제 버튼 동작 추가

--- TodolistApp/script.js
+++ TodolistApp/script.js
@@ -3,11 +3,29 @@ const taskContainer = document.getElementById("task-container");
 
 function addTask() {
   if (inputBox.value === "") {
-    alert("add text");
+    alert(" 할 일을 입력하세요.");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     taskContainer.appendChild(li);
+    // let span = document.createElement("span");
+    // span.innerHTML = "x";
+    // li.appendChild(span);
+    let button = document.createElement("button");
+    button.innerHTML = "x";
+    li.appendChild(button);
   }
   inputBox.value = "";
 }
+
+taskContainer.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("checked");
+    } else if (event.target.tagName === "BUTTON") {
+      event.target.parentElement.remove();
+    }
+  },
+  false
+);

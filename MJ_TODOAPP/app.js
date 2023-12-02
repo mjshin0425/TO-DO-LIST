2b0cd214b41103dc2be526dc6e0c8c7866f40420
mjshin0425 <junis0425@gmail.com>
2023-12-03 00:50:53 +0900

    [UPDATE] 리스트 선택, 삭제 기능 구현

--- MJ_TODOAPP/app.js
+++ MJ_TODOAPP/app.js
@@ -1,19 +1,35 @@
 const inputText = document.getElementById("inputText");
 const addBtn = document.getElementById("addBtn");
+const listContainer = document.getElementById("listContainer");
 
 function addTask() {
   if (inputText.value === "") {
     alert("할 일을 입력하세요.");
   } else {
-    let list = document.createElement("li");
+    const list = document.createElement("li");
     const listContents = document.getElementById("listContainer");
     list.innerHTML = inputText.value;
     listContents.appendChild(list);
-    let span = document.createElement("span");
+
+    const span = document.createElement("span");
     span.innerHTML = "";
     list.appendChild(span);
+    span.classList = "deleteBtn";
+    // const delBtn = document.querySelector("deleteBtn");
+    // delBtn.addEventListener("click", deleteTask);
+
     inputText.value = "";
   }
 }
 
 addBtn.addEventListener("click", addTask);
+
+function handleTask(event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("selected");
+  } else {
+    event.target.parentElement.remove();
+  }
+}
+
+listContainer.addEventListener("click", handleTask);

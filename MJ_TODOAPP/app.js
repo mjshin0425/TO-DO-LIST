5ae925a7d795b3f4f57be577309a2e4a9c1363f1
mjshin0425 <junis0425@gmail.com>
2023-12-05 23:01:05 +0900

    [UPDATE] 목록의 text가 정렬  수정

--- MJ_TODOAPP/app.js
+++ MJ_TODOAPP/app.js
@@ -6,23 +6,44 @@ function addTask() {
   if (inputText.value === "") {
     alert("할 일을 입력하세요.");
   } else {
-    const list = document.createElement("li");
+    // li 생성
     const listContents = document.getElementById("listContainer");
-    list.innerHTML = inputText.value;
-    listContents.appendChild(list);
 
-    // const updatespan = document.createElement("span");
-    // updatespan.innerHTML = "Edit";
-    // list.appendChild(updatespan);
-    // updatespan.classList = "updateBtn";
+    const list = document.createElement("li");
+    list.classList = "task";
+
+    const textbox = document.createElement("div");
+    textbox.classList = "text";
+    textbox.innerHTML = inputText.value;
+
+    listContents.appendChild(list);
+    list.appendChild(textbox);
+
+    // const listInputEl = document.createElement("INPUT");
+    // listInputEl.id = "listInput";
+    // listInputEl.setAttribute("readonly", "readonly");
+    // listInputEl.textContent = inputText.value; //에러다 에러
+    // console.log(inputText.value);
+    // console.log(listInputEl.textContent);
+    // list.appendChild(listInputEl);
+
+    // edit button
+    const editspan = document.createElement("span");
+    editspan.innerHTML = "EDIT";
+    list.appendChild(editspan);
+    editspan.classList = "button";
+    editspan.id = "editBtn";
+    editspan.type = "button";
 
     // delete button
     const deletespan = document.createElement("span");
     deletespan.innerHTML = "DEL";
     list.appendChild(deletespan);
-    deletespan.classList = "deleteBtn";
+    deletespan.classList = "button";
+    deletespan.id = "deleteBtn";
+    deletespan.type = "button";
     const delBtn = document.getElementById("deleteBtn");
-    delBtn.addEventListener("click", deleteTask);
+    // delBtn.addEventListener("click", deleteTask);
 
     inputText.value = "";
   }
@@ -37,10 +58,18 @@ function handleTask(event) {
     event.target.parentElement.remove();
   } else if (event.target.className === "editBtn") {
     const list = document.createElement("LI");
-    const listUpdate = document.createElement("INPUT");
-    const listContents = document.getElementById("listContainer");
-    list.appendChild(listUpdate);
+    const listEditInput = document.createElement("INPUT");
+    list.appendChild(listEditInput); // input을 li에 넣음
+    listEditInput.classList = "editInput";
+    listEditInput.innerHTML = list.innerHTML; // 클릭한 li의 text 내용을 input에 옮기기
+    const listContents = document.getElementById("listContainer"); //
     listContents.appendChild(list);
+
+    // const list = document.createElement("LI");
+    // const listUpdate = document.createElement("INPUT");
+    // const listContents = document.getElementById("listContainer");
+    // list.appendChild(listUpdate);
+    // listContents.appendChild(list);
   }
 }
 

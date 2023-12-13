33cd4419e2018fd63a63508b236b1f5477278033
mjshin0425 <junis0425@gmail.com>
2023-12-13 20:18:02 +0900

    [UPDATE]  TodolistApp delete 버튼 수정

--- TodolistApp/script.js
+++ TodolistApp/script.js
@@ -8,12 +8,15 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     taskContainer.appendChild(li);
-    // let span = document.createElement("span");
-    // span.innerHTML = "x";
-    // li.appendChild(span);
-    let button = document.createElement("button");
-    span.innerHTML = "\00d7";
-    li.appendChild(button);
+
+    let span = document.createElement("span");
+    span.innerHTML = "x";
+    li.appendChild(span);
+
+    // 이모지 삽입 실패
+    //let button = document.createElement("button");
+    //span.innerHTML = "\00d7";
+    //li.appendChild(button);
     saveData();
   }
   inputBox.value = "";
@@ -25,7 +28,7 @@ taskContainer.addEventListener(
     if (event.target.tagName === "LI") {
       event.target.classList.toggle("checked");
       saveData();
-    } else if (event.target.tagName === "BUTTON") {
+    } else if (event.target.tagName === "SPAN") {
       event.target.parentElement.remove();
       saveData();
     }
